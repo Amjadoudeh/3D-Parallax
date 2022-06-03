e167cb6114135abc5433eeda076611c96040ef56
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-06-03 05:36:18 +0200

    First init

---
+++ 3D-Parallax/_D_ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_ParallaxApp.swift
+//  3D-Parallax
+//
+//  Created by Amjad Oudeh on 03.06.22.
+//
+
+import SwiftUI
+
+@main
+struct _D_ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
