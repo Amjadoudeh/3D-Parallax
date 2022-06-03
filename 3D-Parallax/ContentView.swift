849fdccc85fd8cc3159db0a89a095bb87f9cde85
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-06-03 10:52:50 +0200

    Add a card view

--- 3D-Parallax/ContentView.swift
+++ 3D-Parallax/ContentView.swift
@@ -2,8 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 10) {
+            Spacer()
+            Image("SC")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 35, height: 35)
+                .cornerRadius(20)
+            Text("Welcome to SoundCloud")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("SoundCloud is an online audio distribution platform and music...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+        }
+        .padding(.all,20)
+        .frame(height: 400)
+        .background(.orange)
+        .cornerRadius(30)
+        .shadow(radius: 20)
     }
 }
 
